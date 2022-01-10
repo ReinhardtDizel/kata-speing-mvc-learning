98fa31d23ba9a5dc759c98cb6c92ed41f8a7b459
Michael <dize882@gmail.com>
2022-01-10 22:06:30 +0300

    Поправил Контроллер

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,13 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDaoCSV;
 import web.service.CarService;
-import web.service.CarServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +15,12 @@ import java.util.List;
 @RequestMapping("car")
 public class CarController {
 
-    private final CarService carService = new CarServiceImpl(new CarDaoCSV());
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping
     public String carPage(
